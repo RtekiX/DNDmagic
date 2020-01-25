888db18deb5792c61cd4db78c61a81f12a9bf1e0
RtekiX <443294734@qq.com>
2020-01-25 18:45:57 +0800

    data

--- src/test.java
+++ src/test.java
@@ -1,24 +1,70 @@
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
 import java.util.regex.Pattern;
 
 public class test {
+	
+	/*********************************************
+	 * 数据库连接参数
+	 *********************************************/
+	private String JDBC_DRIVER;
+	private String DB_URL;
+	private String USER;
+	private String PASS;
+	Connection conn = null;
+	Statement stmt = null;
 	public static void main(String args[]) throws IOException {
 
-		Pattern pattern = Pattern.compile(
-				" <P class=MsoNormal style=\"LAYOUT-GRID-MODE: char; TEXT-ALIGN: left; MARGIN: 0cm 0cm 0pt; mso-line-height-alt: 0pt; mso-pagination: widow-orphan\" align=left>\r\n"
-						+ "      <SPAN lang=EN-US style=\"FONT-FAMILY: '微软雅黑','sans-serif'\">\r\n"
-						+ "        <o:p>&nbsp;</o:p></SPAN>\r\n" + "    </P>");
-		File file = new File("D:/NAVI/DND/text.txt");
-		FileInputStream inputStream = new FileInputStream(file);
-		int size = inputStream.available();
-		byte[] buffer = new byte[size];
-		inputStream.read(buffer);
-		inputStream.close();
-		String str = new String(buffer, "UTF-8");
-		String[] arr;
-		arr = pattern.split(str);
-		System.out.println(arr[50]);
+	}
+	/**********************
+	 * 连接和关闭数据库
+	 **************************/
+	public void GetConnection() throws ClassNotFoundException, SQLException {
+		/*
+		 * 创建文件流将配置文件jdbc.properties的内容读入dbProperties
+		 */
+		Properties dbProperties = new Properties();
+		InputStream PropertiesIn = test.class.getClassLoader()
+				.getResourceAsStream("jdbc.properties");
+		try {
+			dbProperties.load(PropertiesIn);
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+		// 从dbProperties中读取数据库连接参数
+		JDBC_DRIVER = dbProperties.getProperty("DRIVER");
+		DB_URL = dbProperties.getProperty("URL");
+		USER = dbProperties.getProperty("USER");
+		PASS = dbProperties.getProperty("PASS");
+		// 注册 JDBC 驱动
+		Class.forName(JDBC_DRIVER);
+		// 打开链接
+		System.out.println("连接数据库...");
+		conn = DriverManager.getConnection(DB_URL, USER, PASS);
+		stmt = conn.createStatement();
+	}
+	public void CloseConnection() {
+		try { // 尝试关闭连接
+			if (stmt != null) {
+				stmt.close();
+			}
+			if (conn != null) {
+				conn.close();
+			}
+		} catch (Exception e) {
+			// TODO: handle exception
+			e.printStackTrace();
+		}
+	}
+	public void dataInput() {
+		FileInputStream
 	}
 }
